5babd63712461639d3131260e122e5c46657eaf6
chertpong <chertpong_h@cmu.ac.th>
2016-06-03 10:29:30 +0800

    add modal to marker

--- www/app/mapbegin/mapBegin.ctrl.js
+++ www/app/mapbegin/mapBegin.ctrl.js
@@ -3,145 +3,62 @@
   angular.module('app')
     .controller('mapBeginCtrl', mapBeginCtrl);
 
-  function mapBeginCtrl($scope, $stateParams, Storage, $http, $state, $rootScope, $ionicModal){
+  function mapBeginCtrl($scope, $stateParams, Storage, $http, $state, $rootScope, $ionicModal,$log){
     var data = {}, fn = {};
     $scope.data = data;
     $scope.fn = fn;
     $scope.places=[];
 
-    //Storage.getUserToken().then(function(twitt){
-    //  data.twitt = twitt;
-    //});
-
     var server= "http://52.163.91.205";
     var path = "/api/places";
+    loadMap();
+    var latlngs=[];
 
-    $http.get(server+path)
-      .then(function (response) {
-        console.log(response.data);
-        $scope.places = response.data;
-        Storage.setPlaces(response.data).then(function(){
-         // console.log(Storage.getPlaces());
-        });
-      }).catch(function (err) {
+    function loadMap(){
+      $http.get(server+path)
+        .then(function (response) {
+          $scope.places = response.data;
+          $log.debug('[+]','places are loaded',response.data.length);
+          //TODO : remove after add fake location
+          $scope.places.push({name:'test1',location:{lat:30.648658,lng:104.186639}});
+          $scope.places.push({name:'test2',location:{lat:30.651624,lng:104.186103}});
+          $scope.places.push({name:'test3',location:{lat:30.653036,lng:104.190699}});
+
+          Storage.setPlaces($scope.places).then(function(){
+            setMarkers();
+          });
+        }).catch(function (err) {
         console.log(err);
         $scope.error = true;
       });
-
-  var latlngs=[];
-    //var center;
-    // Get the user's current location
-    navigator.geolocation.getCurrentPosition(
-      function displayCenter(position) {
-        // There is no location set place.location.lat
-        // lat lng for test
-         var latlngs = [
-           new qq.maps.LatLng(30.648658, 104.186639),
-           new qq.maps.LatLng(30.651624, 104.186103),
-           new qq.maps.LatLng(30.653036, 104.190699)
-         ];
-
-        //var infoWin = new qq.maps.InfoWindow({
-        //  map: $rootScope.map
-        //});
-
-        //if (Storage.getPlaces()) {
-        //  for (var j = 0; j < Storage.getPlaces().length; j++) {
-        //
-        //    (function (n) {
-        //        latlngs =[
-        //      center = new qq.maps.LatLng($scope.places.location.lat[n], $scope.places.location.lng[n]);
-        //      //  center= latlngs[n];
-        //        ];
-        //      console.log($scope.places);
-        //    })(j);
-        //  }
-        //}}
-        for (var i = 0; i < latlngs.length; i++) {
-               //alert(latlngs.length);
-            (function (n) {
-            var marker = new qq.maps.Marker({
-             position: latlngs[n],
-              //position: center,
-              map: $rootScope.map
-            });
-            qq.maps.event.addListener(marker, 'click', function () {
-              $ionicModal.fromTemplateUrl('app/twitts/chooseLanguagePopup.html', {
-                scope: $scope,
-                animation: 'slide-in-up'
-              }).then(function (modal) {
-                $scope.modal = modal;
-              });
-              //infoWin.open();
-              //infoWin.setContent('<div style="text-align:center;white-space:'+
-              //  'nowrap;margin:10px;">这是第 ' +
-              //  n + ' 个标注</div>');
-              //infoWin.setPosition(latlngs[n]);
-            });
-          })(
-             i
-           // j
-          );
-        }
-
-
-      });
-
-
-
-
-
-
-
-
-
-    //var latlngs = [
-    //  new qq.maps.LatLng(39.91474,116.37333),
-    //  new qq.maps.LatLng(39.91447,116.39336),
-    //  new qq.maps.LatLng(39.90884,116.41306)
-    //];
-    //
-    //for(var i = 0;i < latlngs.length; i++) {
-    //
-    //  //     alert(latlngs.length);
-    //  (function(n){
-    //    var marker = new qq.maps.Marker({
-    //      position: latlngs[n],
-    //      map: $rootScope.map
-    //    });
-    //    qq.maps.event.addListener(marker, 'click', function() {
-    //      infoWin.open();
-    //      infoWin.setContent('<div style="text-align:center;white-space:'+
-    //        'nowrap;margin:10px;">这是第 ' +
-    //        n + ' 个标注</div>');
-    //      infoWin.setPosition(latlngs[n]);
-    //    });
-    //  })(i);
-    //}
-    //Krit
-    //var center;
-    //// Get the user's current location
-    //navigator.geolocation.getCurrentPosition(
-    //  function displayCenter(position) {
-    //    // There is no location set
-    //    center = new qq.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    //    var marker = new qq.maps.Marker({
-    //      position: center,
-    //      map: $rootScope.map
-    //    });
-    //  });
-
-
-    $scope.linkToBeginMap = function(){
-      console.log('sucess111')
-      $state.go('mapbegin');
     }
-
+    function setMarkers(){
+      $scope.places.forEach(function(place){
+        var geolocation = new qq.maps.LatLng(place.location.lat, place.location.lng);
+        var marker = new qq.maps.Marker({
+          position: geolocation,
+          map: $rootScope.map
+        });
+        $ionicModal.fromTemplateUrl('app/mapbegin/mapViewDetail.html', {
+          scope: $scope,
+          animation: 'slide-in-up'
+        }).then(function (modal) {
+          $scope.modal = modal;
+        });
+        qq.maps.event.addListener(marker, 'click', function () {
+          $scope.modal.show();
+          $scope.place = place;
+        });
+      });
+    }
+    $scope.linkToBeginMap = function(){
+      console.log('sucess111');
+      $state.go('mapbegin');
+    };
 
     $scope.linkToLogin = function(){
-      console.log('sucess')
+      console.log('sucess');
       $state.go('login');
     }
-
   }
 })();
