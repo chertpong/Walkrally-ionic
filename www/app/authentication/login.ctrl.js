7ea3ea05c7ba965b8c319b080e540c6fa7721352
chertpong_h <chertpong_h@cmu.ac.th>
2016-06-07 11:22:38 +0800

    refactor

--- www/app/authentication/login.ctrl.js
+++ www/app/authentication/login.ctrl.js
@@ -3,54 +3,66 @@
   angular.module('app')
     .controller('LoginCtrl', LoginCtrl);
 
-  function LoginCtrl($scope, $state, Storage, $http){
-    var fn= {}, data = {};
+  function LoginCtrl($scope, $state, Storage, $http, C, $log, $ionicPopup) {
+    var fn = {}, data = {};
     $scope.fn = fn;
     $scope.data = data;
+    $scope.error = false;
+    $scope.errorMessage = '';
 
     data.credentials = {
       email: '',
       password: ''
     };
 
-    fn.login = function(credentials){
-      $scope.error = false;
-      //success function
+    $scope.alert = function(){
+      return $ionicPopup.alert({
+        title: 'Error!',
+        template: $scope.errorMessage
+      });
+    };
 
-      if(credentials.email && credentials.password) {
-
-         var server= "http://52.163.91.205";
-          var path = "/auth/local";
-        console.log('credential',data.credentials);
-        $http.post(server+path,data.credentials)
-          .then(function (response) {
-            console.log('login response',response);
-            Storage.setUserToken(response.data.token).then(function(){
-
-              if(response.data.teams){
-                $state.go('teamDetail',{teamId:response.data.teams[0]._id});
-              }else{
+    fn.login = function (credentials) {
+      var path = C.backendUrl + "/auth/local";
+      $http.post(path, data.credentials)
+        .then(function (response) {
+          $log.debug('login response:', response);
+          Storage
+            .setUserToken(response.data.token)
+            .then(function () {
+              return Storage.setUser(response.data.user);
+            })
+            .then(function() {
+              if (response.data.teams) {
+                Storage
+                  .setTeamId(response.data.teams[0]._id)
+                  .then(function() {
+                    $state.go('teamDetail', {teamId: response.data.teams[0]._id});    
+                  });
+              } else {
                 $state.go('twitts');
               }
-
-            });
-
-          }).catch(function (err) {
-          console.log(err);
+          });
+        })
+        .catch(function (err) {
+          $log.debug(err);
           $scope.error = true;
-
+          $scope.errorMessage = err.data.message;
         });
-
-      }
-
-
-      }
-
-    $scope.linkToBeginMap = function(){
-
-      $state.go('mapbegin');
-    }
-
     };
+
+    $scope.linkToBeginMap = function () {
+      $state.go('mapbegin');
+    };
+
+    // Watch for error
+    $scope.$watch('error',function(newValue){
+      if(newValue){
+        $scope.alert().then(function(res) {
+          $scope.error = false;
+        });
+      }
+    });
+  }
 
 })();
